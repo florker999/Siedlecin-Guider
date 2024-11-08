610975f9c52b441dba965d6558407a66c2f97832
CloudfreundMaciek <122986475+CloudfreundMaciek@users.noreply.github.com>
2024-11-08 19:50:09 +0100

    [Modified] main tab
    - changed tab icons
    - removed temporarily cumbersome roof tile

--- components/tower/Tower.tsx
+++ components/tower/Tower.tsx
@@ -1,28 +1,53 @@
-import { StyleSheet, View } from "react-native"
+import { StyleSheet, View } from "react-native";
 import { TowerFloorLink } from "./TowerFloorLink";
 import { TowerRoofLink } from "./TowerRoofLink";
 
-interface IProps {
-
-}
+interface IProps {}
 
 export default function Tower(props: IProps) {
-    return (
-        <View style={styles.towerContainer}>
-            <TowerRoofLink target="/parter" title='Roof' backgroundColor='#004080' titleColor='white' style={styles.noBottomBorder}/>
-            <TowerFloorLink target="/parter" title='III Floor' backgroundColor='#000080' titleColor='white' style={styles.noBottomBorder}/>
-            <TowerFloorLink target="/parter" title='II Floor' backgroundColor='#400080' titleColor='white' style={styles.noBottomBorder}/>
-            <TowerFloorLink target="/parter" title='I Floor' backgroundColor='#800080' titleColor='white' style={styles.noBottomBorder}/>
-            <TowerFloorLink target="/parter" title='Ground Floor' backgroundColor='#800040' titleColor='white' />
-        </View>
-    )
+  return (
+    <View style={styles.towerContainer}>
+      <TowerFloorLink
+        target="/parter"
+        title="Roof"
+        backgroundColor="#004080"
+        titleColor="white"
+        style={styles.noBottomBorder}
+      />
+      <TowerFloorLink
+        target="/parter"
+        title="III Floor"
+        backgroundColor="#000080"
+        titleColor="white"
+        style={styles.noBottomBorder}
+      />
+      <TowerFloorLink
+        target="/parter"
+        title="II Floor"
+        backgroundColor="#400080"
+        titleColor="white"
+        style={styles.noBottomBorder}
+      />
+      <TowerFloorLink
+        target="/parter"
+        title="I Floor"
+        backgroundColor="#800080"
+        titleColor="white"
+        style={styles.noBottomBorder}
+      />
+      <TowerFloorLink
+        target="/parter"
+        title="Ground Floor"
+        backgroundColor="#800040"
+        titleColor="white"
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    towerContainer: {
-
-    },
-    noBottomBorder: {
-        borderBottomWidth: 0
-    }
+  towerContainer: {},
+  noBottomBorder: {
+    borderBottomWidth: 0,
+  },
 });
