4620c35ac4bf778f6f9329cfb3f6f624fe364b48
CloudfreundMaciek <122986475+CloudfreundMaciek@users.noreply.github.com>
2024-11-23 19:16:33 +0100

    [Created] progress bar component

---
+++ components/basic/progressBar.tsx
@@ -0,0 +1,49 @@
+import { Animated, StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+
+interface IProps {
+  progress: number;
+  containerStyle?: StyleProp<ViewStyle>;
+  barStyle?: StyleProp<ViewStyle>;
+}
+
+export default function ProgressBar(props: IProps) {
+  const translateX: `${number}%` = `${-50 * (1 - props.progress)}%`;
+
+  return (
+    <View style={[styles.progressBarContainer, props.containerStyle]}>
+      <Animated.View
+        style={[
+          styles.progressBar,
+          {
+            transform: [
+              { translateX: 0 },
+              { translateX },
+              { scaleX: props.progress },
+            ],
+          },
+          props.barStyle,
+        ]}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  progressBarContainer: {
+    width: "100%",
+    height: 9,
+    borderWidth: 1,
+    borderRadius: 40,
+    borderStyle: "solid",
+    borderColor: "black",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  progressBar: {
+    width: "98%",
+    margin: "auto",
+    borderRadius: 1000,
+    backgroundColor: "black",
+    height: 3,
+  },
+});
